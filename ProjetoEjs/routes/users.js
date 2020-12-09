e2ccc694279b093615bf00e8730a55cceb97400a
Marcelo-Diament <marcelodiament@gmail.com>
2020-12-08 21:36:55 -0300

    feature: novas rotas de users [01]

--- ProjetoEjs/routes/users.js
+++ ProjetoEjs/routes/users.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 var userController = require('../controllers/users')
 
-/* GET users listing. */
 router.get('/', userController.index);
+router.get('/add', userController.add);
+router.get('/edit/:id', userController.edit);
+router.get('/delete/:id', userController.delete);
+router.get('/:id', userController.show);
 
 module.exports = router;
