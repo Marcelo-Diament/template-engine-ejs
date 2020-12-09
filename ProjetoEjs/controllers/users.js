ac3220984f65ccab4524c8af4d0d018dd7070a8c
Marcelo-Diament <marcelodiament@gmail.com>
2020-12-08 21:46:30 -0300

    feature: novos controllers de users [01]

--- ProjetoEjs/controllers/users.js
+++ ProjetoEjs/controllers/users.js
@@ -27,9 +27,34 @@ const usersPlaceholder = [
 
 const controller = {
   index: (req, res, next) => res.render('users', {
-    title: 'Users',
-    message: 'Você está na view users.ejs',
-    users: usersPlaceholder
+    title: `Usuários`,
+    message: `Listagem de todos os usuários`,
+    users: usersPlaceholder,
+    id: null
+  }),
+  add: (req, res, next) => res.render('users', {
+    title: `Novo Usuário`,
+    message: `Adicionar novo usuário`,
+    users: null,
+    id: null
+  }),
+  edit: (req, res, next) => res.render('users', {
+    title: `Editar Usuário ${usersPlacehoder[req.params.id].nome} ${usersPlacehoder[req.params.id].sobrenome}`,
+    message: `Editar os Dados do Usuário #${req.params.id} - ${usersPlacehoder[req.params.id].nome}`,
+    users: usersPlaceholder,
+    id: req.params.id
+  }),
+  delete: (req, res, next) => res.render('users', {
+    title: `Excluir Usuário ${usersPlacehoder[req.params.id].nome} ${usersPlacehoder[req.params.id].sobrenome}`,
+    message: `Excluir os Dados do Usuário #${req.params.id} - ${usersPlacehoder[req.params.id].nome}`,
+    users: usersPlaceholder,
+    id: req.params.id
+  }),
+  show: (req, res, next) => res.render('users', {
+    title: `Usuário ${usersPlacehoder[req.params.id].nome} ${usersPlacehoder[req.params.id].sobrenome}`,
+    message: `Exibindo os Dados do Usuário #${req.params.id} - ${usersPlacehoder[req.params.id].nome}`,
+    users: usersPlaceholder,
+    id: req.params.id
   })
 }
 
