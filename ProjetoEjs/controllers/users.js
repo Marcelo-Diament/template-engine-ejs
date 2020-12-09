c89bf02552900dc3e88619f07ded185b4e4e3b87
Marcelo-Diament <marcelodiament@gmail.com>
2020-12-08 21:27:46 -0300

    feature: partials views [01]

--- ProjetoEjs/controllers/users.js
+++ ProjetoEjs/controllers/users.js
@@ -1,7 +1,35 @@
+const usersPlaceholder = [
+  {
+    "id": 0,
+    "nome": "Fulano",
+    "sobrenome": "da Silva",
+    "avatar": "/images/fulano.png"
+  },
+  {
+    "id": 1,
+    "nome": "Beltrano",
+    "sobrenome": "da Silva",
+    "avatar": "/images/beltrano.png"
+  },
+  {
+    "id": 2,
+    "nome": "Ciclana",
+    "sobrenome": "da Silva",
+    "avatar": "/images/ciclana.png"
+  },
+  {
+    "id": 3,
+    "nome": "Abirosvaldo",
+    "sobrenome": "da Silva",
+    "avatar": "/images/abirosvaldo.png"
+  }
+]
+
 const controller = {
   index: (req, res, next) => res.render('users', {
     title: 'Users',
-    message: 'Você está na view users.ejs'
+    message: 'Você está na view users.ejs',
+    users: usersPlaceholder
   })
 }
 
