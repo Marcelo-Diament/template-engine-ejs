bcfacf60b2f1237d15f5af3e5e91b8acc766da4a
Marcelo-Diament <marcelodiament@gmail.com>
2020-12-10 20:32:05 -0300

    featt push origin feature/03-users-parte-II

--- ProjetoEjs/controllers/users.js
+++ ProjetoEjs/controllers/users.js
@@ -30,31 +30,36 @@ const controller = {
     title: `Usuários`,
     message: `Listagem de todos os usuários`,
     users: usersPlaceholder,
-    id: null
+    id: null,
+    topImage: '/images/users.jpg'
   }),
   add: (req, res, next) => res.render('users', {
     title: `Novo Usuário`,
     message: `Adicionar novo usuário`,
     users: null,
-    id: null
+    id: null,
+    topImage: '/images/users.jpg'
   }),
   edit: (req, res, next) => res.render('users', {
     title: `Editar Usuário ${usersPlaceholder[req.params.id].nome} ${usersPlaceholder[req.params.id].sobrenome}`,
     message: `Editar os Dados do Usuário #${req.params.id} - ${usersPlaceholder[req.params.id].nome}`,
     users: usersPlaceholder,
-    id: req.params.id
+    id: req.params.id,
+    topImage: `${usersPlaceholder[req.params.id].avatar}`
   }),
   delete: (req, res, next) => res.render('users', {
     title: `Excluir Usuário ${usersPlaceholder[req.params.id].nome} ${usersPlaceholder[req.params.id].sobrenome}`,
     message: `Excluir os Dados do Usuário #${req.params.id} - ${usersPlaceholder[req.params.id].nome}`,
     users: usersPlaceholder,
-    id: req.params.id
+    id: req.params.id,
+    topImage: `${usersPlaceholder[req.params.id].avatar}`
   }),
   show: (req, res, next) => res.render('users', {
     title: `Usuário ${usersPlaceholder[req.params.id].nome} ${usersPlaceholder[req.params.id].sobrenome}`,
     message: `Exibindo os Dados do Usuário #${req.params.id} - ${usersPlaceholder[req.params.id].nome}`,
     users: usersPlaceholder,
-    id: req.params.id
+    id: req.params.id,
+    topImage: `${usersPlaceholder[req.params.id].avatar}`
   })
 }
 
--- ProjetoEjs/routes/index.js
+++ ProjetoEjs/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express + EJS',
+    message: 'Página Inicial do nosso Projeto',
+    topImage: 'https://expressjs.com/images/express-facebook-share.png'
+  });
 });
 
 module.exports = router;
