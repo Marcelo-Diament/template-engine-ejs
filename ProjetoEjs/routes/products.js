c795285f6a602074c8191d381285cd46e9742626
Marcelo-Diament <marcelodiament@gmail.com>
2020-12-10 21:50:53 -0300

    bugfix: ajustando rotas

--- ProjetoEjs/routes/products.js
+++ ProjetoEjs/routes/products.js
@@ -7,6 +7,6 @@ router.get('/add', controller.add)
 router.get('/edit/:id', controller.edit)
 router.get('/delete/:id', controller.delete)
 router.get('/categoria/:categoria', controller.filter)
-router.get(':id', controller.show)
+router.get('/:id', controller.show)
 
 module.exports = router
